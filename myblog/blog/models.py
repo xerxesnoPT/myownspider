53c2562e530ab678175823675ef7858394d01faa
xerxesnoPT <346287244@qq.com>
2017-07-13 17:29:47 +0800

    django blog

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+import datetime
 
 # Create your models here.
 class Question(models.Model):
@@ -7,6 +8,9 @@ class Question(models.Model):
     pub_date = models.DateField('date published')
     def __str__(self):
         return self.question_text
+    def was_published_recently(self):
+        lasttime = timezone.now() - datetime.timedelta(days=1)
+        return timezone.now()>=self.pub_date>=lasttime
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,6 +1,67 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Question
+from .models import Choice
+from django.views import generic
+from django.urls import reverse
+from django.shortcuts import get_object_or_404,render
+from django.utils import timezone
+from django.template import loader
 
 # Create your views here.
-def index(request):
-    return HttpResponse('hi my dear !!!')
+# def index(request):
+#     question_list = Question.objects.order_by('-pub_date')[:5]
+#     # template = loader.get_template('blog/index.html')
+#     context = {
+#         'question_list': question_list,
+#     }
+#     # return HttpResponse(template.render(context,request))
+#     return render(request, 'blog/index.html', context)
+#
+# def detail(request,question_id):
+#     # try:
+#     #     question = Question.objects.get(pk=question_id)
+#     # except Question.DoesNotExist:
+#     #     raise Http404('question does not exist')
+#     # return render(request, 'blog/detail.html', {'question': question})
+#     question = get_object_or_404(Question,pk=question_id)
+#     return render(request, 'blog/detail.html',{'question':question})
+#
+# def result(request,question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request,'blog/results.html', {'question':question})
+class IndexView(generic.ListView):
+    template_name = 'blog/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now()
+                                       ).order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'blog/detail.html'
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'blog/results.html'
+
+def vote(request,question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        choice_id = request.POST['choice']
+        selected_choice = question.choice_set.get(pk=choice_id)
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'blog/detail.html',{
+            'question': question,
+            'error_message': 'You didnt select a choice.',
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('blog:results', args=(question_id,)))
+
+
+
